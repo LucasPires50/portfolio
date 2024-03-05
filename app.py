a2fcf6f30113c9a2f2668cb2a92b914fd3dfc0a8
LucasPires50 <luc_50@outlook.com>
2024-03-05 16:22:35 -0300

    feat: config env

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template, redirect
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+
+import os
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = 'code'
+
+mail_sttings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": os.getenv("EMAIL"),
+    "MAIL_PASSWORD": os.getenv("EMAIL_PASSWORD"),
+}
+
 
 @app.route('/')
 def index():
